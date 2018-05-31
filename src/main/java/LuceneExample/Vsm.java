0d2cc4394c12f612d3ca23b8d54dafea486c74fc
MagnetoWang <1527672216@qq.com>
2018-05-31 10:27:54 +0800

    向量空间模型，主要是计算cosA。分别求两个文档的norm距离，然后求交集。再从交集中计算点乘然后求和。

--- src/main/java/LuceneExample/Vsm.java
+++ src/main/java/LuceneExample/Vsm.java
@@ -5,6 +5,16 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+/***
+ * @see. 向量空间模型 来计算关键词和文档之间的相关度
+ * 线性代数的简单模型
+ * 词组的权重不是二元的
+ * 文档和查询之间的相似度取值是连续的
+ * 允许根据文档之间可能的相关性进行排序
+ * 允许局部匹配
+ *
+ * 核心算法 ： cosA 的计算
+ */
 public class Vsm {
     public static double calCosSim(Map<String, Double> v1,
                                    Map<String, Double> v2){
@@ -14,7 +24,9 @@ public class Vsm {
         Set<String> both= new HashSet<>();
         both.addAll(v1Keys);
         both.retainAll(v2Keys);
+        System.out.println("两个文档之间的交集");
         System.out.println(both);
+//        sclar是点乘然后求和
         for (String str1 : both) {
             sclar += v1.get(str1) * v2.get(str1);
         }
@@ -24,11 +36,21 @@ public class Vsm {
         for (String str2:v2.keySet()){
             norm2+=Math.pow(v2.get(str2),2);
         }
+        //感觉计算有些问题 10：22
+
+        //没有问题 10：23
         similarity=sclar/Math.sqrt(norm1*norm2);
+
         System.out.println("sclar:"+sclar);
-        System.out.println("norm1:"+norm1);
-        System.out.println("norm2:"+norm2);
+//        文档1的范式距离
+        System.out.println("norm1:" + norm1);
+//        文档2的范式距离
+        System.out.println("norm2:" + norm2);
+
+
+//        similarity=sclar*norm2/Math.sqrt(sclar*norm2);
         System.out.println("similarity:"+similarity);
+//        System.out.println("similarity2:"+similarity);
         return similarity;
     }
     public static void main(String[] args) {
